247e1677f10cd3726aaaebcb8df1d97e5a6e2516
HXear <767251930@qq.com>
2022-12-04 22:44:00 +0800

    Update FMEDA API

--- org.osate.iso26262.fmea/src/org/osate/iso26262/fmea/FMEDAPI.java
+++ org.osate.iso26262.fmea/src/org/osate/iso26262/fmea/FMEDAPI.java
@@ -1,10 +1,11 @@
 package org.osate.iso26262.fmea;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
+import org.osate.aadl2.errormodel.FaultTree.Event;
 import org.osate.aadl2.instance.ComponentInstance;
 import org.osate.ui.dialogs.Dialog;
 
@@ -20,8 +21,7 @@ public class FMEDAPI {
 		// 构造结构树
 		fb.Construct_structure_tree(ci);
 		// 同时构造故障网与功能网
-		fb.BuildFailureAndFuncNet(fb.root_component);
-
+		fb.BuildFailureAndFuncNet();
 	}
 
 	public List<String> Get_Safety_Goal() {
@@ -32,28 +32,27 @@ public class FMEDAPI {
 		return result;
 	}
 
+	// old
 	public List<ComponentInstance> Get_Calcul_Instance(ComponentInstance ci, String sg) {
-		List<ComponentInstance> result = new ArrayList<ComponentInstance>();
 
 		boolean findsg = false;
-		Map<String, Boolean> alreadyin = new HashMap<String, Boolean>();
+		Set<FailureElement> failureset = new HashSet<FailureElement>();
+		Set<ComponentInstance> componentset = new HashSet<ComponentInstance>();
 
 		for (Function fci : fb.root_component.functions.values()) {
 			if (fci.funcname.equals(sg)) {
 				findsg = true;
 
-				List<FailureMode> leaffmlist = new ArrayList<FailureMode>();
-				for (FailureMode fmi : fci.ref_fail_modes) {
-					fmi.getLeaf(leaffmlist);
+				for (FailureElement fmi : fci.ref_fail_modes) {
+					fmi.getCauseLeaf(failureset);
 				}
-				for (FailureMode leaffmi : leaffmlist) {
-					if (alreadyin.get(leaffmi.ref_component.ci.getFullName()) == null) {
-						result.add(leaffmi.ref_component.ci);
-						alreadyin.put(leaffmi.ref_component.ci.getFullName(), true);
-					}
+				for (FailureElement leaffmi : failureset) {
+					componentset.add(leaffmi.ref_component.ci);
 				}
+				break;
 			}
 		}
+		List<ComponentInstance> result = new ArrayList<ComponentInstance>(componentset);
 		if (findsg == false) {
 			Dialog.showInfo("Get_Calcul_Instance",
 					"Can't find Safety Goal \"" + sg + "\" in Component \"" + ci.getName() + "\"");
@@ -61,6 +60,73 @@ public class FMEDAPI {
 		}
 		if (result.size() == 0) {
 			Dialog.showInfo("Get_Calcul_Instance", "Can't find related sub component in Safety Goal \"" + sg + "\"");
+			return null;
+		}
+		return result;
+	}
+
+	// new 1
+	public List<ComponentInstance> Get_Calcul_Instance(String sg) {
+
+		boolean findsg = false;
+		Set<FailureElement> failureset = new HashSet<FailureElement>();
+		Set<ComponentInstance> componentset = new HashSet<ComponentInstance>();
+
+		for (Function fci : fb.root_component.functions.values()) {
+			if (fci.funcname.equals(sg)) {
+				findsg = true;
+
+				for (FailureElement fmi : fci.ref_fail_modes) {
+					fmi.getCauseLeaf(failureset);
+				}
+				for (FailureElement leaffmi : failureset) {
+					componentset.add(leaffmi.ref_component.ci);
+				}
+				break;
+			}
+		}
+		List<ComponentInstance> result = new ArrayList<ComponentInstance>(componentset);
+		if (findsg == false) {
+			Dialog.showInfo("Get_Calcul_Instance",
+					"Can't find Safety Goal \"" + sg + "\" in Component \"" + ci.getName() + "\"");
+			return null;
+		}
+		if (result.size() == 0) {
+			Dialog.showInfo("Get_Calcul_Instance", "Can't find related sub component in Safety Goal \"" + sg + "\"");
+			return null;
+		}
+		return result;
+	}
+
+	// new 2
+	public List<Event> Get_Calcul_Event(String sg) {
+
+		boolean findsg = false;
+		Set<FailureElement> failureset = new HashSet<FailureElement>();
+		Set<Event> eventset = new HashSet<Event>();
+
+		for (Function fci : fb.root_component.functions.values()) {
+			if (fci.funcname.equals(sg)) {
+				findsg = true;
+
+				for (FailureElement fmi : fci.ref_fail_modes) {
+					fmi.getCauseLeaf(failureset);
+				}
+				for (FailureElement leaffmi : failureset) {
+					eventset.add(leaffmi.ref_event);
+				}
+				break;
+			}
+		}
+		List<Event> result = new ArrayList<Event>(eventset);
+		if (findsg == false) {
+			Dialog.showInfo("Get_Calcul_Instance",
+					"Can't find Safety Goal \"" + sg + "\" in Component \"" + ci.getName() + "\"");
+			return null;
+		}
+		if (result.size() == 0) {
+			Dialog.showInfo("Get_Calcul_Instance", "Can't find related sub component in Safety Goal \"" + sg + "\"");
+			return null;
 		}
 		return result;
 	}
